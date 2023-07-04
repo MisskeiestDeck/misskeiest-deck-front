65fe78c7f8b6780412658b6e6dd5a4a8d3a86cd4
猫宮ぴあの / Piano Nekomiya <kotone.olin1010@gmail.com>
2023-07-04 17:13:20 +0900

    layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,19 @@
 <script>
     import '../style.css';
 </script>
-  
-<slot />
+
+<div id="body">
+
+<div id="pc-navbar">
+    <p>MD</p>
+</div>
+
+<main>
+    <slot />
+</main>
+
+<div id="rightbar">
+    <p>Notification</p>
+</div>
+
+</div>
